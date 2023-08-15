90f135fe46ec74ed7dda4f266df68e2d7a7d82ed
elhadj cire <bahmamadoucire14@gmail.com>
2023-08-15 13:38:01 +0000

    ajout des icons personnalisé

--- src/icons/ArrowLeft.tsx
+++ src/icons/ArrowLeft.tsx
@@ -1,4 +1,4 @@
-const MoonIcon = ({className}:{className?:string}) => {
+const ArrowLeft = ({ className }: { className?: string }) => {
     return (
         <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -10,10 +10,10 @@ const MoonIcon = ({className}:{className?:string}) => {
             <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
             />
         </svg>
     );
 };
 
-export default MoonIcon;
+export default ArrowLeft;
