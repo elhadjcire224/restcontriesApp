ffe638bd38d7ed511adaa212d1e6d66bde3fb880
elhadj cire <bahmamadoucire14@gmail.com>
2023-09-12 22:38:37 +0000

    ajout du scrollToUp

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,28 +1,32 @@
 //content.tsx
-import {  useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useTheme } from "../context/useTheme";
 import { cn } from "../utils/cn";
 import FilterBar from "./FilterBar";
 import SearchBar from "./SearchBar";
-import {  useFilter } from "../hooks/useFilter";
+import { useFilter } from "../hooks/useFilter";
 import CountriesList from "./ContriesList";
 import { Country, useCountries } from "../context/CountryContent";
 import CountryDetails from "./CountryDetails";
 import Squeletons from "./Squeletons";
+import { ArrowUp } from "lucide-react";
 
-function filter(countries:Country[],regionNames:string[]){
-    
-    return countries?.filter(country => regionNames.includes(country.region));
+function filter(countries: Country[], regionNames: string[]) {
+    return countries?.filter((country) => regionNames.includes(country.region));
 }
 
 const Content = () => {
     const { isDark } = useTheme();
-    const { regions, handleClick,regionNames } = useFilter();
-    const { countries,isLoading} = useCountries();
+    const { regions, handleClick, regionNames } = useFilter();
+    const { countries, isLoading } = useCountries();
     const [search, setSearch] = useState("");
-    
+
     const [filteredCountries, setFilteredCountries] = useState(countries);
-    const [selectedCountry, setSelectedCountry] = useState<Country|null>(null);
+    const [selectedCountry, setSelectedCountry] = useState<Country | null>(
+        null
+    );
+
+    const ref = useRef<HTMLElement>(null)
 
     useEffect(() => {
         setFilteredCountries(countries);
@@ -32,7 +36,7 @@ const Content = () => {
         if (regionNames.length == 0) {
             setFilteredCountries(countries);
         } else {
-            setFilteredCountries(filter(countries,regionNames))
+            setFilteredCountries(filter(countries, regionNames));
         }
     };
 
@@ -41,7 +45,10 @@ const Content = () => {
     }, [regions]);
 
     const handleSearch = (query: string) => {
-        const searchableCountries = regionNames.length == 0 ? countries : filter(countries,regionNames);
+        const searchableCountries =
+            regionNames.length == 0
+                ? countries
+                : filter(countries, regionNames);
         const result = searchableCountries?.filter((country: Country) => {
             const capital = country.capital ? country.capital[0] : "";
             return (
@@ -61,16 +68,21 @@ const Content = () => {
         handleSearch(search);
     }, [search]);
 
-    
-
     return (
         <main
-            className={cn('content',
+            
+            
+            className={cn(
+                "content relative",
                 "bg-light-gray px-3 min-h-screen",
                 isDark && "bg-dark-bg"
             )}>
             {selectedCountry == null ? (
-                <section className={cn("py-5 flex flex-col gap-5","sm:flex-row justify-between mx-4")}>
+                <section
+                    className={cn(
+                        "py-5 flex flex-col gap-5",
+                        "sm:flex-row justify-between mx-4"
+                    )}>
                     <SearchBar search={search} setSearch={setSearch} />
                     <FilterBar regions={regions} handleClick={handleClick} />
                 </section>
@@ -84,11 +96,33 @@ const Content = () => {
                             countries={filteredCountries}
                         />
                     ) : (
-                        <div className="bg-red-500 text-white p-8 rounded-lg  border border-red-800">desolé rien ne correspond à vos recherches</div>
+                        <div className="bg-red-500 text-white p-8 rounded-lg  border border-red-800">
+                            desolé rien ne correspond à vos recherches
+                        </div>
                     )
-                ) : (<CountryDetails country={selectedCountry} setSelectedCountry={setSelectedCountry}/>
+                ) : (
+                    <CountryDetails
+                        country={selectedCountry}
+                        setSelectedCountry={setSelectedCountry}
+                    />
                 )
-            ) : <Squeletons/>}
+            ) : (
+                <Squeletons />
+            )}
+            {!isLoading && !selectedCountry && filteredCountries?.length !== 0 && (<button
+                className={cn(
+                    "p-2 flex rounded-full bg-dark-bg text-white  items-center justify-center fixed",
+                    " bottom-8 right-8 transition",
+                    isDark && "bg-white",
+
+
+                )}
+                onClick={() => scrollTo({top:0,behavior:"smooth"})}
+                >
+                {" "}
+                <ArrowUp className={cn(isDark && "fill-white text-dark-bg")}/>
+            </button>)}
+            
         </main>
     );
 };
